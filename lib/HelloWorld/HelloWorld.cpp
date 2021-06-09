b2257cbadcbe35668e743ec1ef0e2052e2cade33
AntonGhobryal <antonusama.ghobryal@mail.polimi.it>
2021-06-09 22:52:50 +0200

    Code optimization as suggested by the comments\nadded size()

--- lib/HelloWorld/HelloWorld.cpp
+++ lib/HelloWorld/HelloWorld.cpp
@@ -16,14 +16,9 @@ using namespace llvm;
  */
 namespace {
 	//count how many functions are read
-	static int ins_count = 0;
-	//count how many basic blocks
-	static int bb_count = 0;
+	
 	/* subclass of FunctionPass
 	 * operates on a single function at a time
-	 */
-	/* TODO: 1. implement the printing of the number of basic blocks of such function
-	 * 	 2. implement the printing of the number of instructions for such function
 	 */
 	struct Hello : public FunctionPass {
 		/* Declaration of pass identifier used by llvm
@@ -31,33 +26,29 @@ namespace {
 		 * information
 		 */
 		static char ID;
-		int ins_count = 0;
-		int bb_count = 0;
+
 		Hello() : FunctionPass(ID) {}
+		
 		bool runOnFunction(Function &F) override{
 			/* Here's where we should do our things
 			 * on the function. In this case, we're
 			 * only printing out the function name
 			 */
+			int i = 1;
 			errs() << "Hello: ";
 			errs().write_escaped(F.getName()) << '\n';
+			
 			/*counting the number of Basic Blocks and
 			 * the number of instructions for each
 			 * Basic Block
 			 */
+			errs() << "There are " << F.size() << " Basic Blocks"
+				<< '\n';
 			for(BasicBlock &BB : F){
-				bb_count++;
-				errs() << "Basic Block n" <<
-					bb_count << ": ";
-				for(Instruction &I : BB)
-					ins_count++;
-				errs() << 
-				"the number of instructions is"
-				<< ins_count << "instructions\n";
-				ins_count = 0;
+				errs() << "Basic Block n" << i << " has " 
+					<< BB.size() << " instructions" << '\n';
+				i++;
 			}
-			errs() << "The number of basic blocks is "
-			       << bb_count << '\n';
 			return false;
 		}
 	};
@@ -79,20 +70,3 @@ static RegisterPass<Hello> X("hello", "Hello World Pass",
 			      false /* Only looks at CFG */,
 			      false /* Analysis Pass */);
 
-/* If there exists a pipeline of passes we might choose to
- * execute the specified one before any optimization through
- * PassManagerBuilder::EP_EarlyAsPossible, or after Link Time
- * Optimizations through PassManagerBuilder::EP_FullLinkTime-
- * OptimizationLast
- */
-/*
-static llvm::RegisterStandardPasses Y(
-		llvm::PassManagerBuilder::EP_EarlyAsPossible,
-		[](const llvm::PassManagerBuilder &Builder,
-		   llvm::legacy::PassManagerBase &PM) {
-		   PM.add(new Hello());});
-*/
-static RegisterStandardPasses Y(
-    PassManagerBuilder::EP_EarlyAsPossible,
-    [](const PassManagerBuilder &Builder,
-       legacy::PassManagerBase &PM) { PM.add(new Hello()); });
